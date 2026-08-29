87874a73fdec42ea9d0abe2bde23139dafaab8d0
bernish12 <bernish8888@gmail.com>
2026-08-29 20:07:31 +0530

    Deploy: add login.js

--- login.js
+++ login.js
@@ -53,3 +53,13 @@ if (loginForm) {
 } else {
     document.getElementById('loginBtn').addEventListener('click', performLogin);
 }
+
+const togglePassword = document.querySelector('#togglePassword');
+const passwordField = document.querySelector('#password');
+if (togglePassword && passwordField) {
+    togglePassword.addEventListener('click', function (e) {
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+        this.classList.toggle('fa-eye-slash');
+    });
+}
